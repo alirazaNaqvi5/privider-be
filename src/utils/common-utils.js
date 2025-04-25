86472e6a4d8b0f599ee5b6e26e518977bb930da2
Ali Naqvi <91022754+alirazaNaqvi5@users.noreply.github.com>
2025-04-25 21:30:22 +0500

    fix

--- src/utils/common-utils.js
+++ src/utils/common-utils.js
@@ -21,11 +21,6 @@ const sendErrorResp = (error, req, resp) => {
 };
 
 
-// function to sum two numbers
-const sumTwoNumbers = (a, b) => a + b;
-
-const sumThreeNumbers = (a, b, c) => a + b + c;
-
 const getCurrentDate = () => { return moment().format('YYYY-MM-DD'); }
 
 // create a function to multiply 2 numbers
